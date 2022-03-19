4ec899c873142cad00ac6d3d25b00977be974d31
nonnonstop <42905588+nonnonstop@users.noreply.github.com>
2022-03-19 11:10:35 +0900

    Replace SettingsActivity

--- app/src/main/java/space/aioilight/tsubonofuta/AppConfig.kt
+++ app/src/main/java/space/aioilight/tsubonofuta/AppConfig.kt
@@ -4,12 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Environment
+import androidx.preference.PreferenceDataStore
 import de.robv.android.xposed.XSharedPreferences
 import java.io.File
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-class AppConfig {
+class AppConfig : PreferenceDataStore {
     var hideInlineAd by BooleanPreference("inline", true)
     var hideThreadAd by BooleanPreference("thread", true)
     var hidePastLogAd by BooleanPreference("past_log", true)
@@ -44,15 +45,101 @@ class AppConfig {
         }
     }
 
-    private open class BasePreference<T>(
+    @SuppressLint("SetWorldReadable")
+    private fun makeWorldReadable() {
+        if (!privateMode)
+            return
+
+        try {
+            val packageDir = File(
+                Environment.getDataDirectory(),
+                "data/${BuildConfig.APPLICATION_ID}/"
+            )
+            val prefDir = File(
+                packageDir,
+                "shared_prefs/"
+            )
+            val prefFile = File(
+                prefDir,
+                "${FILE_NAME}.xml"
+            )
+            prefFile.setReadable(true, false)
+            prefDir.setExecutable(true, false)
+            packageDir.setExecutable(true, false)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    @SuppressLint("ApplySharedPref")
+    override fun putString(key: String, value: String?) {
+        pref.edit().putString(key, value).commit()
+        makeWorldReadable()
+    }
+
+    @SuppressLint("ApplySharedPref")
+    override fun putStringSet(key: String, values: MutableSet<String>?) {
+        pref.edit().putStringSet(key, values).commit()
+        makeWorldReadable()
+    }
+
+    @SuppressLint("ApplySharedPref")
+    override fun putInt(key: String, value: Int) {
+        pref.edit().putInt(key, value).commit()
+        makeWorldReadable()
+    }
+
+    @SuppressLint("ApplySharedPref")
+    override fun putLong(key: String, value: Long) {
+        pref.edit().putLong(key, value).commit()
+        makeWorldReadable()
+    }
+
+    @SuppressLint("ApplySharedPref")
+    override fun putFloat(key: String, value: Float) {
+        pref.edit().putFloat(key, value).commit()
+        makeWorldReadable()
+    }
+
+    @SuppressLint("ApplySharedPref")
+    override fun putBoolean(key: String, value: Boolean) {
+        pref.edit().putBoolean(key, value).commit()
+        makeWorldReadable()
+    }
+
+    override fun getString(key: String, defValue: String?): String? {
+        return pref.getString(key, defValue)
+    }
+
+    override fun getStringSet(key: String, defValues: MutableSet<String>?): MutableSet<String>? {
+        return pref.getStringSet(key, defValues)
+    }
+
+    override fun getInt(key: String, defValue: Int): Int {
+        return pref.getInt(key, defValue)
+    }
+
+    override fun getLong(key: String, defValue: Long): Long {
+        return pref.getLong(key, defValue)
+    }
+
+    override fun getFloat(key: String, defValue: Float): Float {
+        return pref.getFloat(key, defValue)
+    }
+
+    override fun getBoolean(key: String, defValue: Boolean): Boolean {
+        return pref.getBoolean(key, defValue)
+    }
+
+    private sealed class BasePreference<T>(
         private val name: String,
         private val defaultValue: T,
-        private val getter: SharedPreferences.(String, T) -> T,
-        private val setter: SharedPreferences.Editor.(String, T) -> SharedPreferences.Editor
+        private val getter: AppConfig.(String, T) -> T,
+        private val setter: AppConfig.(String, T) -> Unit
     ) : ReadWriteProperty<AppConfig, T> {
         override fun getValue(thisRef: AppConfig, property: KProperty<*>): T {
             return try {
-                thisRef.pref.getter(name, defaultValue)
+                thisRef.getter(name, defaultValue)
             } catch (e: Exception) {
                 e.printStackTrace()
                 defaultValue
@@ -61,31 +148,8 @@ class AppConfig {
 
         @SuppressLint("CommitPrefEdits")
         override fun setValue(thisRef: AppConfig, property: KProperty<*>, value: T) {
-            thisRef.pref.edit().setter(name, value).commit()
-            makeWorldReadable(thisRef)
-        }
-
-        @SuppressLint("SetWorldReadable")
-        private fun makeWorldReadable(thisRef: AppConfig) {
-            if (!thisRef.privateMode)
-                return
-
             try {
-                val packageDir = File(
-                    Environment.getDataDirectory(),
-                    "data/${BuildConfig.APPLICATION_ID}/"
-                )
-                val prefDir = File(
-                    packageDir,
-                    "shared_prefs/"
-                )
-                val prefFile = File(
-                    prefDir,
-                    "${FILE_NAME}.xml"
-                )
-                prefFile.setReadable(true, false)
-                prefDir.setExecutable(true, false)
-                packageDir.setExecutable(true, false)
+                thisRef.setter(name, value)
             } catch (e: Exception) {
                 e.printStackTrace()
             }
@@ -96,7 +160,7 @@ class AppConfig {
         BasePreference<Boolean>(
             name,
             defaultValue,
-            SharedPreferences::getBoolean,
-            SharedPreferences.Editor::putBoolean
+            AppConfig::getBoolean,
+            AppConfig::putBoolean
         )
 }
--- app/src/main/java/space/aioilight/tsubonofuta/SettingsActivity.kt
+++ app/src/main/java/space/aioilight/tsubonofuta/SettingsActivity.kt
@@ -1,207 +1,33 @@
 package space.aioilight.tsubonofuta
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.animation.rememberSplineBasedDecay
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
-import space.aioilight.tsubonofuta.ui.theme.DynamicColor
-import space.aioilight.tsubonofuta.ui.theme.Typography
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
 
-class SettingsActivity : ComponentActivity() {
+class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SettingScreen()
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SettingScreen() {
-    val decayAnimationSpec = rememberSplineBasedDecay<Float>()
-    val scrollBehavior = remember(decayAnimationSpec) {
-        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(decayAnimationSpec)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
     }
 
-    DynamicColor {
-        // A surface container using the 'background' color from the theme
-        Scaffold(
-            modifier = Modifier
-                .fillMaxSize()
-                .nestedScroll(scrollBehavior.nestedScrollConnection),
-            content = {
-                SettingsContent()
-            },
-            topBar = {
-                SettingsTopBar(scrollBehavior)
-            }
-        )
-    }
-}
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            preferenceManager.preferenceDataStore = AppConfig.newInstanceForModule(requireContext())
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-@Composable
-fun SettingsContent() {
-    val scrollState = rememberScrollState()
-    val config = AppConfig.newInstanceForModule(LocalContext.current)
-    Column(
-        Modifier.verticalScroll(scrollState)
-    ) {
-        SettingsSwitch(
-            title = stringResource(id = R.string.settings_thread_title),
-            description = stringResource(id = R.string.settings_thread_desc),
-            value = config.hideThreadAd
-        ) {
-            config.hideThreadAd = it
-        }
-        SettingsSwitch(
-            title = stringResource(id = R.string.settings_inline_title),
-            description = stringResource(id = R.string.settings_inline_desc),
-            value = config.hideInlineAd
-        ) {
-            config.hideInlineAd = it
-        }
-        SettingsSwitch(
-            title = stringResource(id = R.string.settings_past_log_title),
-            description = stringResource(id = R.string.settings_past_log_desc),
-            value = config.hidePastLogAd
-        ) {
-            config.hidePastLogAd = it
-        }
-        Spacer(modifier = Modifier.height(32.dp))
-        GitHub()
-        Status()
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SettingsTopBar(topAppBarScrollBehavior: TopAppBarScrollBehavior) {
-    LargeTopAppBar(
-        title = { Text(text = stringResource(id = R.string.app_name)) },
-        scrollBehavior = topAppBarScrollBehavior
-    )
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SettingsSwitch(
-    title: String,
-    description: String,
-    value: Boolean,
-    onChanged: (Boolean) -> Unit
-) {
-    val checkedState = remember { mutableStateOf(value) }
-    Row(
-        Modifier
-            .clickable {
-                checkedState.value = !checkedState.value
-                onChanged(checkedState.value)
-            }
-            .fillMaxWidth()
-            .padding(24.dp),
-        Arrangement.SpaceBetween,
-    ) {
-        Column(
-            Modifier.weight(1f)
-        ) {
-            Text(
-                text = title,
-                style = Typography.subtitle1
-            )
-            if (description.isNotEmpty()) {
-                Text(
-                    text = description,
-                    style = Typography.caption
-                )
-            }
-        }
-        Checkbox(checked = checkedState.value, onCheckedChange = null)
-    }
-}
-
-@Composable
-fun Status() {
-    val title = stringResource(
-        id = R.string.settings_status_title,
-        stringResource(id = R.string.app_name),
-        BuildConfig.VERSION_NAME
-    )
-    val desc = stringResource(id = R.string.settings_status_desc)
-    val context = LocalContext.current
-    Row(
-        Modifier
-            .clickable {
-                val intent = Intent(context, OssLicensesMenuActivity::class.java)
-                context.startActivity(intent)
-            }
-            .fillMaxWidth()
-            .padding(24.dp),
-        Arrangement.SpaceBetween,
-    ) {
-        Column(
-            Modifier.weight(1f)
-        ) {
-            Text(
-                text = title,
-                style = Typography.subtitle1
-            )
-            Text(
-                text = desc,
-                style = Typography.caption
+            findPreference<Preference>("status")?.title = resources.getString(
+                R.string.settings_status_title,
+                resources.getString(R.string.app_name),
+                BuildConfig.VERSION_NAME
             )
         }
     }
-}
-
-@Composable
-fun GitHub() {
-    val uri = stringResource(id = R.string.repository_url)
-    val context = LocalContext.current
-    Row(
-        Modifier
-            .clickable {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
-                context.startActivity(intent)
-            }
-            .fillMaxWidth()
-            .padding(24.dp),
-        Arrangement.SpaceBetween,
-    ) {
-        Column(
-            Modifier.weight(1f)
-        ) {
-            Text(
-                text = stringResource(id = R.string.settings_github_title),
-                style = Typography.subtitle1
-            )
-            Text(
-                text = stringResource(id = R.string.settings_github_desc),
-                style = Typography.caption
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SettingScreen()
 }
