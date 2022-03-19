a774d17167657b6d68eb957017d6c4cd9b93c7ab
nonnonstop <42905588+nonnonstop@users.noreply.github.com>
2022-03-19 21:08:52 +0900

    Move advanced settings

--- app/src/main/java/space/aioilight/tsubonofuta/SettingsActivity.kt
+++ app/src/main/java/space/aioilight/tsubonofuta/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package space.aioilight.tsubonofuta
 
 import android.os.Bundle
+import android.view.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.nonnonstop.tsubonofuta.BuildConfig
@@ -11,25 +13,85 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportFragmentManager.addOnBackStackChangedListener {
+            supportActionBar?.setDisplayHomeAsUpEnabled(
+                supportFragmentManager.backStackEntryCount > 0
+            )
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                replace(R.id.settings, SettingsFragment())
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                supportFragmentManager.popBackStack()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setHasOptionsMenu(true)
+        }
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            val config = AppConfig.newInstanceForModule(requireContext())
-            preferenceManager.preferenceDataStore = config
+            preferenceManager.preferenceDataStore = AppConfig.newInstanceForModule(requireContext())
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
             findPreference<Preference>("status")?.title = resources.getString(
                 R.string.settings_status_title,
                 resources.getString(R.string.app_name),
                 BuildConfig.VERSION_NAME
             )
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View {
+            activity?.title = preferenceScreen.title
+            return super.onCreateView(inflater, container, savedInstanceState)
+        }
+
+        override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+            inflater.inflate(R.menu.settings_activity, menu)
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.advanced -> {
+                    parentFragmentManager.commit {
+                        replace(R.id.settings, AdvancedSettingsFragment())
+                        addToBackStack(null)
+                    }
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    class AdvancedSettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            preferenceManager.preferenceDataStore = AppConfig.newInstanceForModule(requireContext())
+            setPreferencesFromResource(R.xml.advanced_preferences, rootKey)
+        }
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View {
+            activity?.title = preferenceScreen.title
+            return super.onCreateView(inflater, container, savedInstanceState)
         }
     }
 }
